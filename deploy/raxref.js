bf7d66e7c8011aaf787ab0589d826914f3cc6bbc
dr-berlin <info1@raisin.de>
2009-03-18 15:24:23 +0100

    File splitting implemented. Files are splitted into chunks that make loading pain smaller for xreffing.

--- deploy/raxref.js
+++ deploy/raxref.js
@@ -146,39 +146,6 @@ jQuery(function($) {
             return xrefList;
         };
 
-        var iFetchResultCount= 0;
-
-        var fetchResults= function(xrefList, fetchedFunc, finishedFunc) {
-            var iCurrentCount= ++iFetchResultCount;
-            var _next= function() {
-                if (xrefList.length == 0) {
-                    finishedFunc();
-                    return;
-                }
-                var xref= xrefList.shift();
-                jQuery.ajax({
-                    url: files_path + "/file" + xref.file_no + ".html?" + Math.random(),
-                    dataType: "html",
-                    complete: function(res, status) {
-
-                        // If new fetchResult was called, stop bothering.
-                        // I couldn't make Ajaxthingy.abort() working, properly,
-                        // so do it this way. 
-                        if (iCurrentCount < iFetchResultCount) {
-                            console.warn("Ajax call aborted");
-                            return;
-                        }
-
-                        if (status == "success" || status == "notmodified") {
-                            fetchedFunc(xref, res.responseText);
-                        };
-                        _next();
-                    }
-                });
-            };
-            _next();
-        };
-
         var htmlize= function(text) {
             return (text || '').replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
         };
@@ -187,10 +154,28 @@ jQuery(function($) {
             return htmlize(text).replace(/\//g, '\/<wbr \/>');
         };
 
-        var file= function(file_no) {
-            if (files[file_no]) return files[file_no];
-            return ['n/a', 'n/a', 'n/a'];
+        var filename= function(file_no) {
+            if (files[file_no]) return files[file_no][2];
+            return 'n/a';
         };
+
+        var file_by_line= function(file_no, line_no) {
+
+            // FIXME: ugly. make this 0 based
+            line_no--;
+
+            var fileinfo= files[file_no];
+            var length= fileinfo[1];
+            if (line_no >= length) return null;
+
+            var file_split= fileinfo[0];
+            var line_ofs= line_no % file_split;
+            var start= line_no - line_ofs;
+
+            return [ files_path + "/file" + file_no + "-" + (start / file_split) + ".html", line_ofs ];
+        };
+
+        var showXrefCount= 0;
 
         var showXref= function(token) {
 
@@ -203,7 +188,7 @@ jQuery(function($) {
             var result= [];
             for (var xrefList_i in xrefList) {
                 var xref= xrefList[xrefList_i];
-                result.push("<ol><li><h1>" + htmlize_filename(file(xref.file_no)[2]) + "</h1></li>");
+                result.push("<ol><li><h1>" + htmlize_filename(filename(xref.file_no)) + "</h1></li>");
                 for (var line_no_i in xref.line_nos) {
                     var line_no= xref.line_nos[line_no_i];
                     var spanId= 'q' + id + '-' + xref.file_no + '-' + line_no;
@@ -214,62 +199,125 @@ jQuery(function($) {
             }
             showText("Xref for '" + token + "'", "<div class='code xref'>" + result.join("\n") + "</div>");
 
-            fetchResults(xrefList,
-                function(xref, file_c) {
-                    var lines= file_c.split(/\n/);
-                    for (var i in xref.line_nos) {
-                        var line_no= xref.line_nos[i];
+            var fetchedFunc= function(file_no, line_no, line) {
+                line= line.replace(/^<li[^>]+>/, '').replace(/<\/li>$/, '');      // remove <li> and </li>
+                var spanId= 'q' + id + '-' + file_no + '-' + line_no;
+                $('#' + spanId).html(line);
+            };
 
+            var part_lines= null;
 
-                        // cache-problematik: loesung: erste zeile checken ob file stimmt, ansonsten nochmal holen mit _rnd=sdf
-                        if (lines[line_no] == undefined) {
-                            // console.log(lines);
-                            console.log(lines.length, line_no);
+            var fetchedFunc2= function(file_no, line_no, line_ofs) {
+                fetchedFunc(file_no, line_no, line_ofs < part_lines.length ? part_lines[line_ofs] : line_ofs + " >= " + part_lines.length + " ???");
+            };
+
+            var currentCount= ++showXrefCount;
+            var last_filepos= [ '', -1 ];
+            var xref= null;
+            var line_nos= [];
+
+            var _next_line_no= function() {
+                for (;;) {
+                    var line_no= line_nos.shift();
+                    if (line_no == null) {
+                        xref= xrefList.shift();
+                        if (xref == null) {
+                            // finishedFunc();
+                            return;
                         }
-
-                        var line;
-                        if (line_no >= lines.length) {
-                            line= line_no + " > " + lines.length + " ???";
-                        }
-                        else {
-                            line= lines[line_no].replace(/^<li[^>]+>/, '').replace(/<\/li>$/, '');      // remove <li> and </li>
-                        }
-
-                        var spanId= 'q' + id + '-' + xref.file_no + '-' + line_no;
-                        $('#' + spanId).html(line);
+                        line_nos= xref.line_nos;
+                        line_no= line_nos.shift();
                     }
-                },
-                function() {
-                    // showText("Xref for '" + token + "'", "<div class='code xref'>" + result.join("\n") + "</div>");
+
+                    var file_no= xref.file_no;
+                    var filepos= file_by_line(file_no, line_no);
+                    if (filepos == null) {
+
+                        // FIXME: cache-problematik: loesung: erste zeile checken ob file stimmt, ansonsten nochmal holen mit "cache: false"
+                        // Laenger nicht mehr aufgetaucht, weiter beobachten...
+                        fetchedFunc(file_no, line_no, "<li>Kaputt</li>");
+                        return;
+                    }
+
+                    // Found in different file? Fetch it with Ajax.
+                    if (filepos[0] != last_filepos[0]) {
+                        last_filepos= filepos;
+                        jQuery.ajax({
+                            url: filepos[0],
+                            dataType: "html",
+                            complete: function(res, status) {
+
+                                // If showXref was called again, stop bothering.
+                                // I couldn't make Ajaxthingy.abort() working, properly,
+                                // so do it this way. 
+                                if (currentCount < showXrefCount) {
+                                    console.warn("Ajax call aborted");
+                                    return;
+                                }
+
+                                if (status != "success" && status != "notmodified") {
+                                    // FIXME: Do something...
+                                    return;
+                                };
+
+                                part_lines= res.responseText.split(/\n/);
+
+                                fetchedFunc2(file_no, line_no, filepos[1]);
+                                _next_line_no();
+                            }
+                        });
+                        return;
+                    }
+
+                    fetchedFunc2(file_no, line_no, filepos[1]);
                 }
-            );
+            };
+
+            _next_line_no();
         };
 
         var showFile= function(file_no, line_no) {
-        jQuery.ajax({
-            url: files_path + "/file" + file_no + ".html",
-            dataType: "html",
-            complete: function(res, status){
-            if (status == "success" || status == "notmodified") {
-                        line_no= line_no > 0 ? line_no : 0;
-                        var $body= showText(
-                            "File '" + htmlize(file(file_no)[2]) + "'",
-                            ["<h1>", htmlize_filename(file(file_no)[2]), "</h1>",
-                                "<div class='code'><ol>",
-                                res.responseText,
-                                "</ol></div>"].join(''),
-                            true);
-                        $body.scrollTo(line_no <= 6 ? 0 : "li:nth-child(" + (line_no - 6)+ ")");
-                        if (line_no) {
-                            $body.find("li:nth-child(" + line_no + ")")
-                                .css("backgroundColor", "#FF0")
-                                .animate({ "backgroundColor": "#FFF" }, 3000)
-                            ;
-                        }
-                    };
-                    return this;
+            var length= files[file_no][1];
+            var file_split= files[file_no][0];
+            var start= 0;
+            var part_no= 0;
+            var collected= [ "<h1>", htmlize_filename(filename(file_no)), "</h1><div class='code'><ol>" ];
+
+            var _done= function() {
+                line_no= line_no > 0 ? line_no : 0;
+                collected.push("</ol></div>");
+                var $body= showText(
+                    "File '" + htmlize(filename(file_no)) + "'",
+                    collected.join(''),
+                    true);
+                $body.scrollTo(line_no <= 6 ? 0 : "li:nth-child(" + (line_no - 6)+ ")");
+                if (line_no) {
+                    $body.find("li:nth-child(" + line_no + ")")
+                        .css("backgroundColor", "#FF0")
+                        .animate({ "backgroundColor": "#FFF" }, 3000)
+                    ;
                 }
-            });
+            };
+
+            var _next= function() {
+                jQuery.ajax({
+                    url: files_path + "/file" + file_no + "-" + part_no + ".html",
+                    dataType: "html",
+                    complete: function(res, status){
+                        if (status != "success" && status != "notmodified") {
+                            // FIXME: Do something!
+                            return;
+                        }
+                        collected.push(res.responseText),
+                        part_no++;
+                        start += file_split;
+                        if (start >= length) return _done();
+
+                        _next();
+                    }
+                });
+            };
+            _next();
         };
 
         var showProject= function() {
@@ -281,9 +329,6 @@ jQuery(function($) {
 
             showText(htmlize(project_title), "<div class='sections'>" + result.join("") + "</div>");
         };
-
-        // var findProject= function(sectionsName) {
-        // }
 
         var showSection= function(section_i) {
             var section= sections[section_i];
